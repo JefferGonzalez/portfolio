55d13dd4c83661d61bef076077211002ba2c1241
Jeffer <jefferd2016@gmail.com>
2022-07-18 16:17:14 -0500

    Add flag component

---
+++ src/components/flag/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function flag({country, alt, lang}) {
+  return (
+    <img
+        alt={alt}
+        src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${country}.svg`}
+        width="38"
+        data-bs-toggle="tooltip"
+        data-bs-placement="left"
+        title={lang}
+    />
+  )
+}
+
+export default flag;
