241bdede5824cd2b25872b2abc4c4fc415075f32
Jeffer <jefferd2016@gmail.com>
2022-08-09 16:45:55 -0500

    Delete olded components

--- src/components/spinner/index.js
+++ src/components/spinner/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-export default function Spinner({
-  width = '6rem' , height = '6rem'
-}) {
+export default function Spinner({ width = "6rem", height = "6rem" }) {
   return (
-    <div
-      className="spinner-grow text-success"
-      style={{ width: width, height: height }}
-      role="status"
-    >
-      <span className="visually-hidden">Loading...</span>
+    <div className="position-absolute top-50 start-50 translate-middle">
+      <div className="h-100 text-center text-dark">
+        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+          <main className="px-3 h-100">
+            <div className="d-flex justify-content-center">
+              <div
+                className="spinner-grow text-success"
+                style={{ width: width, height: height }}
+                role="status"
+              >
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
