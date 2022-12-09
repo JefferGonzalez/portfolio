8db7022c00bacce11f23f4b9efd564ed8434da6c
JefferGonzalez <jefferd2016@gmail.com>
2022-12-09 09:58:21 -0500

    Update skill page

--- src/json/skills.js
+++ src/json/skills.js
@@ -1,105 +1,105 @@
 const JSONSkills = [
-    {
-        "Studied" : [
-            {
-                "id": 1,
-                "name" : "HTML",
-                "url-image": require('./../resources/img/skills/HTML-5.png'),
-            },
-            {
-                "id": 2,
-                "name" : "CSS",
-                "url-image": require('./../resources/img/skills/CSS-3.png'),
-            },
-            {
-                "id": 3,
-                "name" : "JavaScript",
-                "url-image": require('./../resources/img/skills/JavaScript.png'),
-            },
-            {
-                "id": 4,
-                "name" : "PHP",
-                "url-image": require('./../resources/img/skills/PHP.png'),
-            },
-            {
-                "id": 5,
-                "name" : "Java",
-                "url-image": require('./../resources/img/skills/Java.png'),
-            },
-            {
-                "id": 6,
-                "name" : "SQL",
-                "url-image": require('./../resources/img/skills/SQL.png'),
-            },
-            {
-                "id": 7,
-                "name" : "Git",
-                "url-image": require('./../resources/img/skills/GIT.png'),
-            },
-            {
-                "id": 8,
-                "name" : "jQuery",
-                "url-image": require('./../resources/img/skills/JQUERY.png'),
-            },
-            {
-                "id": 9,
-                "name" : "MySQL",
-                "url-image": require('./../resources/img/skills/MYSQL.png'),
-            },
-            {
-                "id": 10,
-                "name" : "Microsoft SQL Server",
-                "url-image": require('./../resources/img/skills/SQL_SERVER.png'),
-            },
-            {
-                "id": 11,
-                "name" : "PostgreSQL",
-                "url-image": require('./../resources/img/skills/PostgreSQL.png'),
-            },
-            {
-                "id": 12,
-                "name" : "NPM",
-                "url-image": require('./../resources/img/skills/NPM.png'),
-            },
-            {
-                "id": 13,
-                "name" : "Composer",
-                "url-image": require('./../resources/img/skills/COMPOSER.png'),
-            },
-            {
-                "id": 14,
-                "name" : "Bootstrap",
-                "url-image": require('./../resources/img/skills/BOOTSTRAP.png'),
-            },
-        ],
-        "Learning" : [
-            {
-                "id": 1,
-                "name" : "Python",
-                "url-image": require('./../resources/img/skills/PYTHON.png'),
-            },
-            {
-                "id": 2,
-                "name" : "React",
-                "url-image": require('./../resources/img/skills/REACT.png'),
-            },
-            {
-                "id": 3,
-                "name" : "Material UI",
-                "url-image": require('./../resources/img/skills/MUI.png'),
-            },
-            {
-                "id": 4,
-                "name" : "Node.js",
-                "url-image": require('./../resources/img/skills/NODEJS.png'),
-            },
-            {
-                "id": 5,
-                "name" : "Spring Boot",
-                "url-image": require('./../resources/img/skills/SPRING-BOOT.png'),
-            },
-        ]
-    }
-]
+  {
+    Studied: [
+      {
+        id: 1,
+        name: "HTML",
+        "url-image": require("./../resources/img/skills/HTML-5.png"),
+      },
+      {
+        id: 2,
+        name: "CSS",
+        "url-image": require("./../resources/img/skills/CSS-3.png"),
+      },
+      {
+        id: 3,
+        name: "Bootstrap",
+        "url-image": require("./../resources/img/skills/BOOTSTRAP.png"),
+      },
+      {
+        id: 4,
+        name: "jQuery",
+        "url-image": require("./../resources/img/skills/JQUERY.png"),
+      },
+      {
+        id: 5,
+        name: "React",
+        "url-image": require("./../resources/img/skills/REACT.png"),
+      },
+      {
+        id: 6,
+        name: "JavaScript",
+        "url-image": require("./../resources/img/skills/JavaScript.png"),
+      },
+      {
+        id: 7,
+        name: "NodeJS",
+        "url-image": require("./../resources/img/skills/NODEJS.png"),
+      },
+      {
+        id: 8,
+        name: "PHP",
+        "url-image": require("./../resources/img/skills/PHP.png"),
+      },
+      {
+        id: 9,
+        name: "SQL",
+        "url-image": require("./../resources/img/skills/SQL.png"),
+      },
+      {
+        id: 10,
+        name: "Git",
+        "url-image": require("./../resources/img/skills/GIT.png"),
+      },
+      {
+        id: 11,
+        name: "MySQL",
+        "url-image": require("./../resources/img/skills/MYSQL.png"),
+      },
+      {
+        id: 12,
+        name: "Microsoft SQL Server",
+        "url-image": require("./../resources/img/skills/SQL_SERVER.png"),
+      },
+      {
+        id: 13,
+        name: "PostgreSQL",
+        "url-image": require("./../resources/img/skills/PostgreSQL.png"),
+      },
+      {
+        id: 14,
+        name: "NPM",
+        "url-image": require("./../resources/img/skills/NPM.png"),
+      },
+      {
+        id: 15,
+        name: "Composer",
+        "url-image": require("./../resources/img/skills/COMPOSER.png"),
+      },
+    ],
+    Learning: [
+      {
+        id: 1,
+        name: "Python",
+        "url-image": require("./../resources/img/skills/PYTHON.png"),
+      },
+      {
+        id: 2,
+        name: "Material UI",
+        "url-image": require("./../resources/img/skills/MUI.png"),
+      },
+      {
+        id: 3,
+        name: "Java",
+        "url-image": require("./../resources/img/skills/Java.png"),
+      },
+      {
+        id: 4,
+        name: "Spring Boot",
+        "url-image": require("./../resources/img/skills/SPRING-BOOT.png"),
+      },
+    ],
+  },
+];
 
 export default JSONSkills;
